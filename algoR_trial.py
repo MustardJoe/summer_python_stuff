91f8a142f0d576d8e7734e488aedf4c568c08f2b
Jonathan Forney <jonathan.forney@gmail.com>
2020-06-28 17:12:18 -0700

    ☠️remembering some python oop principals

--- algoR_trial.py
+++ algoR_trial.py
@@ -1,3 +1,19 @@
 introMsg = "Hello World!"
-
 print(introMsg)
+
+class Ship:
+    def __init__(self, draft, crew):
+        self.draft = draft
+        self.crew = crew
+    def is_worth_it(self):
+        print(self.draft - (1.5 * self.crew))
+
+boat = Ship(15, 10)
+
+boat.is_worth_it()
+
+
+
+
+
+
