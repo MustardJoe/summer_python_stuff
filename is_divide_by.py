ead0c36e325644ff4790f77f0b5448361fe206da
Jonathan Forney <jonathan.forney@gmail.com>
2020-07-07 13:50:53 -0700

    more python fundamentals

---
+++ is_divide_by.py
@@ -0,0 +1,4 @@
+def is_divide_by(number, a, b):
+  print(number % a == 0)
+  print(number % b == 0)
+  return (number % a == 0) and (number % b == 0)
---
+++ list_includes.py
@@ -0,0 +1,7 @@
+def include(arr,item):
+  if item in arr:
+    print('good for you')
+    return True
+  else:
+    print('bad... no good for you')
+    return False
